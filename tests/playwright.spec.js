001235b7490450a1f3d78863f3a0697593baaca5
Jessica Francavilla <jessica.francavilla86@gmail.com>
2025-01-03 10:23:47 -0300

    update playwright

--- tests/playwright.spec.js
+++ tests/playwright.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Should log in, add, edit, and delete a post, and log out', async ({ page }) => {
+test('Should log in', async ({ page }) => {
 	// Paso 1: Loguearse
 	await page.goto('http://localhost:3000/home');
 	await page.waitForTimeout(2000);
@@ -8,50 +8,9 @@ test('Should log in, add, edit, and delete a post, and log out', async ({ page }
 	await page.fill('input[name="password"]', '123456');
 	await page.click('#submit');
 	await expect(page).toHaveURL(/.*\/blog/);
-	await page.waitForTimeout(2000);
+	await page.waitForTimeout(1000);
 
-	// Paso 2: Agregar post
 	await page.click('#create');
-	await page.waitForTimeout(1000);
-	await page.screenshot({ path: 'create-post.png' });
-	await page.fill('input[name="title"]', 'Testing Playwright');
-	await page.fill('input[name="author"]', 'John Smith');
-	await page.fill(
-		'textarea[name="description"]',
-		`What you'll learn: The solutions Playwright provides for testing. The features of Playwright App, Playwright Cloud, UI Coverage, and Playwright Accessibility. Our mission and what we believe in. Key differences between Playwright and other testing tools`
-	);
-	await page.click('input[name="author"]');
-	await page.click('#upload');
-	const inputFile = page.locator('input[type="file"]');
-	await inputFile.setInputFiles('test-image.jpg');
-	await page.waitForTimeout(1000);
-	await page.click('#save');
-	await page.waitForTimeout(1000);
-	await expect(page.locator('text=Playwright')).toBeVisible();
 
-	// Paso 3: Editar post
-	await page.click('#more');
-	await page.click('#edit-post');
-	await page.waitForTimeout(1000);
-	await page.fill('input[name="title"]', '');
-	await page.fill('input[name="title"]', 'Testing Playwright + Edited');
-	await page.click('#save');
-	await page.waitForTimeout(2000);
-	await page.reload();
-	await page.waitForTimeout(2000);
-
-	await expect(page.locator('text=Testing Playwright + Edited')).toBeVisible();
-
-	// Paso 4: Borrar post
-	await page.locator('.post-card').first().click();
-	await page.waitForTimeout(1000);
-	await page.click('#more');
-	await page.click('#delete-post');
-	await page.screenshot({ path: 'delete-post.png' });
-	await expect(page.locator('text=Confirmar eliminación')).toBeVisible();
-	await page.click('#confirm');
-	await page.waitForTimeout(2000);
-	await page.reload();
-	await page.waitForTimeout(2000);
-	await expect(page.locator('text=Testing Playwright')).not.toBeVisible();
+	await page.waitForTimeout(3000);
 });
